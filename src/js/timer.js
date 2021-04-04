b0aa370f8578baf1e5b11f854fa52d4fb2a3528a
Sergey-Proskurnin <proskurnin@ua.fm>
2021-04-04 20:29:22 +0300

    Add start project files

---
+++ src/js/timer.js
@@ -0,0 +1,15 @@
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+const CountdownTimer = {
+    
+}
+
+
+
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+  });
